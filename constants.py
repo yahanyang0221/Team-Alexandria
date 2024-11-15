74aec1d1d5212ec1177911fa250e3dc9927d2a5a
KunHeeLisaLee <kunheel@andrew.cmu.edu>
2024-11-15 15:20:32 -0500

    Add files via upload
    .py file w column names

---
+++ constants.py
@@ -0,0 +1,65 @@
+
+# tabl 1: institution
+institution_columns = [
+    "UNITID", "OPEID", "INSTNM", "ACCREDAGENCY", "INST_CONTROL", "REGION",
+    "MAIN", "NUMBRANCH", "PREDEG", "HIGHDEG", "ZIP", "CITY", "ADDR",
+    "LATITUDE", "LONGITUD", "FIPS", "CBSA", "CBSATYPE", "CSA", "C21BASIC",
+    "C21IPUG", "C21IPGRD", "C21UGPRF", "C21ENPRF", "C21ZSZET", "CREATED_AT",
+    "UPDATED_AT"
+]
+
+hd_institution_columns = [
+    "UNITID", "OPEID", "INSTNM", "LATITUDE", "LONGITUD", "FIPS", "CBSA",
+    "CBSATYPE", "CSA", "C21BASIC", "C21IPUG", "C21IPGRD", "C21UGPRF",
+    "C21ENPRF", "C21SZSET"]
+
+score_institution_columns = [
+    "UNITID", "OPEID", "INSTNM", "ACCREDAGENCY", "CONTROL", "REGION",
+    "MAIN", "NUMBRANCH", "HIGHDEG", "ZIP", "CITY", "ADDR", "PREDDEG",
+    "HIGHDEG"]
+
+# table 2 : loan
+loan_columns = ["LOAN_ID", "OPEID", "YEAR", "CDR2", "CDR3", "DBRR5_FED_UG_RT"]
+
+hd_loan_columns = ["OPEID", "YEAR"]
+
+score_loan_columns = ["OPEID", "YEAR", "CDR2", "CDR3", "DBRR5_FED_UG_RT"]
+
+
+# table 3:  graduation
+graduation_columns = [
+    "GRADUATION_ID", "OPEID", "YEAR", "UGNONDS", "GRADS", "MD_EARN_WNE_4YR"]
+
+hd_graduation_columns = ["OPEID", "YEAR"]
+
+score_graduation_columns = [
+    "OPEID", "UGNONDS", "GRADS", "MD_EARN_WNE_4YR", "YEAR"]
+
+# table 4: faculty
+faculty_columns = ["FACULTY_ID", "OPEID", "AVGFACSAL", "YEAR"]
+
+hd_faculty_columns = ["OPEID"]
+
+score_faculty_columns = ["OPEID", "AVGFACSAL"]
+
+# table 5: admission
+admission_columns = [
+    "ADMISSION_ID", "OPEID", "YEAR", "ADM_RATE", "SATVRMID",
+    "SATMTMID", "SATWRMID"]
+
+hd_admission_columns = ["OPEID", "YEAR"]
+
+score_admission_columns = [
+    "OPEID", "YEAR", "ADM_RATE", "SATVRMID",
+    "SATMTMID", "SATWRMID", "ACTCMMID", "ADMCON7"]
+
+# table 6:
+tuition_columns = [
+    "TUITION_ID", "OPEID", "YEAR", "TUITIONFEE_IN", "TUITIONFEE_OUT",
+    "TUITIONFEE_PROG", "TUITFTE", "COSTT4_A", "COSTT4_P"]
+
+hd_tuition_columns = ["OPEID", "YEAR"]
+
+score_tuition_columns = [
+    "OPEID", "YEAR", "TUITIONFEE_IN", "TUITIONFEE_OUT",
+    "TUITIONFEE_PROG", "TUITFTE", "COSTT4_A", "COSTT4_P"]
