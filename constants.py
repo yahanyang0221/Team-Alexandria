33410ba0aa3b7f8a21f6c4360fa3ac7da8af601f
yahanyang0221 <163084934+yahanyang0221@users.noreply.github.com>
2024-11-30 11:27:53 -0500

    Add files via upload

--- constants.py
+++ constants.py
@@ -20,35 +20,23 @@ score_institution_columns = [
 
 # table 2 : loan
 loan_columns = ["LOAN_ID", "OPEID", "YEAR", "CDR2", "CDR3", "DBRR5_FED_UG_RT"]
-
-hd_loan_columns = ["OPEID", "YEAR"]
-
 score_loan_columns = ["OPEID", "YEAR", "CDR2", "CDR3", "DBRR5_FED_UG_RT"]
 
 
 # table 3:  graduation
 graduation_columns = [
     "GRADUATION_ID", "OPEID", "YEAR", "UGNONDS", "GRADS", "MD_EARN_WNE_4YR"]
-
-hd_graduation_columns = ["OPEID", "YEAR"]
-
 score_graduation_columns = [
     "OPEID", "UGNONDS", "GRADS", "MD_EARN_WNE_4YR", "YEAR"]
 
 # table 4: faculty
 faculty_columns = ["FACULTY_ID", "OPEID", "AVGFACSAL", "YEAR"]
-
-hd_faculty_columns = ["OPEID"]
-
-score_faculty_columns = ["OPEID", "AVGFACSAL"]
+score_faculty_columns = ["OPEID", "YEAR", "AVGFACSAL"]
 
 # table 5: admission
 admission_columns = [
     "ADMISSION_ID", "OPEID", "YEAR", "ADM_RATE", "SATVRMID",
     "SATMTMID", "SATWRMID"]
-
-hd_admission_columns = ["OPEID", "YEAR"]
-
 score_admission_columns = [
     "OPEID", "YEAR", "ADM_RATE", "SATVRMID",
     "SATMTMID", "SATWRMID", "ACTCMMID", "ADMCON7"]
@@ -57,9 +45,6 @@ score_admission_columns = [
 tuition_columns = [
     "TUITION_ID", "OPEID", "YEAR", "TUITIONFEE_IN", "TUITIONFEE_OUT",
     "TUITIONFEE_PROG", "TUITFTE", "COSTT4_A", "COSTT4_P"]
-
-hd_tuition_columns = ["OPEID", "YEAR"]
-
 score_tuition_columns = [
     "OPEID", "YEAR", "TUITIONFEE_IN", "TUITIONFEE_OUT",
     "TUITIONFEE_PROG", "TUITFTE", "COSTT4_A", "COSTT4_P"]
